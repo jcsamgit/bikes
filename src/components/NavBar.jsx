d6e6411ececdac2b5440d610aec1c29d85d54206
Julio Castañeira <jcv.gerald@gmail.com>
2022-04-21 00:51:10 -0300

    limpieza de codigo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,43 +10,40 @@ function NavBar(){
     const [{cart},dispatch]= useStateValue()
     return (
         <>
-        {/* <div className='navContent'> */}
             <h1 ><Link className="fantasia" to='/'>Bikeshop</Link></h1>
-        <nav className="navbar">
-            <ul className="menu">
-                <li>
-                    <Link to='/'>Inicio</Link>
-                </li>
-                <li>
-                    <Link to='#'>Categoria</Link>
-                    <ul >
-                        <li><Link to={'/category/bicicletas'}>Bicicletas</Link> </li>
-                        <li><Link to={'/category/accesorios'}>Accesorios</Link></li>
-                    </ul>
-                </li>
-                <li>
-                    <li><Link to={'/contacto'}>Contacto</Link></li>
-                    {/* <a href="#">Contacto</a> */}
-                </li>
-                <li>
+            <nav className="navbar">
+                <ul className="menu">
+                    <li>
+                        <Link to='/'>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to='#'>Categoria</Link>
+                        <ul >
+                            <li><Link to={'/category/bicicletas'}>Bicicletas</Link> </li>
+                            <li><Link to={'/category/accesorios'}>Accesorios</Link></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <li><Link to={'/contacto'}>Contacto</Link></li>
+                    </li>
+                    <li>
 
-                    <Link to={'/carrito'}> 
-                        <span className='navCart'>
-                            <CartWidget/>
-                                {cart?.length > 0 &&   
-                                    <p className='contadorCarrito'>
-                                        {cart?.length}
-                                    </p>
-                                }
-                        </span>
-                    </Link> 
+                        <Link to={'/carrito'}> 
+                            <span className='navCart'>
+                                <CartWidget/>
+                                    {cart?.length > 0 &&   
+                                        <p className='contadorCarrito'>
+                                            {cart?.length}
+                                        </p>
+                                    }
+                            </span>
+                        </Link> 
 
-                </li>
-                    
-            </ul>
-        </nav>
-         
-         {/* </div> */}
+                    </li>
+                        
+                </ul>
+            </nav>
+            
          </>
 
     )
